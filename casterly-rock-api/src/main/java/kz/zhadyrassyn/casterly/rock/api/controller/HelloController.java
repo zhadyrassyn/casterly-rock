04539983b9f38ba4d5787e9e195b0a95df81ce7d
Daniyar Zhadyrassyn <dzhadyrassyn@alfabank.kz>
2019-04-18 12:07:11 +0600

    initial setup

---
+++ casterly-rock-api/src/main/java/kz/zhadyrassyn/casterly/rock/api/controller/HelloController.java
@@ -0,0 +1,15 @@
+package kz.zhadyrassyn.casterly.rock.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world";
+    }
+}
