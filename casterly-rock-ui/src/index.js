51e0a7506a32853e598895f9d21b88401a911f79
Daniyar Zhadyrassyn <dzhadyrassyn@alfabank.kz>
2019-05-15 12:09:25 +0600

    add front ui. header

---
+++ casterly-rock-ui/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from './Router';
+import App from './App'
+
+import { createStore, applyMiddleware} from "redux";
+
+ReactDOM.render(<App />, document.getElementById('root'));
